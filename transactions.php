94734266430dd758d0f1e6581824e117475fe446
Kumar Vaibhav <k.vaibhav2908@gmail.com>
2018-12-12 18:51:14 -0800

    closes #8

--- transactions.php
+++ transactions.php
@@ -1,85 +1,11 @@
 <?php
 //Written by Kumar Vaibhav
 //010110295
+include 'navigation.php';
 
-if (!isset($_COOKIE['user'])) {
-    echo "<script>alert(\"You must be logged in\");</script>";
-    echo "<script>window.location = \"home.php\";</script>";
-    exit;
-}
+echo '<div style="margin-top: 7%" class="container-fluid">
+<div class="row text-center" style="display:flex; flex-wrap:wrap;">';
 
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Transactions</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-    
-</head>
-<body>
-<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Pixi</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-       <?php
-        if (!isset($_COOKIE["user"])) {
-            echo <<<_END
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-        <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-            <li class="nav-item active">
-                <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="register.php">Register <span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-_END;
-        } else {
-            echo <<<_END
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="images_all.php">Images <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="upload.php">Upload <span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-        <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-            <li class="nav-item dropdown">
-                <a class="nav-item nav-link dropdown-toggle mr-md-2" href="#" id="bd-versions" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Account
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="bd-versions">
-                    <a class="dropdown-item" href="transactions.php">My Purchases</a>
-                    <a class="dropdown-item" href="logout.php">Logout</a>
-                    <a class="dropdown-item" style="color:red" href="delete_account.php">Delete Account</a>
-                </div>
-            </li>
-        </ul>
-_END;
-        }
-        ?>
-  </div>
-</nav>
-
-<div style="margin-top: 7%" class="container-fluid">
-<div class="row text-center" style="display:flex; flex-wrap:wrap;">
-<?php 
 
 
  // Create connection to MySQL
